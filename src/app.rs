b523371c037116ff0375e29cc194638c43932cbf
Magi <paoloimbrmagi@gmail.com>
2025-12-10 19:19:42 +0100

    changed from tailywind colors to ratatui::style::Color

--- src/app.rs
+++ src/app.rs
@@ -1,20 +1,21 @@
-use color_eyre::{Result};
+use color_eyre::Result;
 use ratatui::{
-    DefaultTerminal, Frame, crossterm::event::{self, Event, KeyCode, KeyEventKind},
-    layout::{Constraint, Layout, Rect}, style::{Style, palette::tailwind},
-    text::Text, widgets::{
-        Block, Paragraph, StatefulWidget, Widget
-    }
+    crossterm::event::{self, Event, KeyCode, KeyEventKind},
+    layout::{Constraint, Layout, Rect},
+    style::{Color, Style},
+    text::Text,
+    widgets::{Block, Paragraph, StatefulWidget, Widget},
+    DefaultTerminal, Frame,
 };
 use tui_big_text::{BigText, PixelSize};
-use wordle_tui::{WordleGrid, get_daily_word};
+use wordle_tui::{get_daily_word, WordleGrid};
 
 /// Struct for the main data for the App.
 pub struct App {
     solution: String,
     has_won: bool,
     text_box: String,
-    content: WordleGrid
+    content: WordleGrid,
 }
 
 impl Default for App {
@@ -23,7 +24,7 @@ impl Default for App {
             solution: String::new(),
             has_won: false,
             text_box: String::from("(Esc) quit"),
-            content: WordleGrid::default()
+            content: WordleGrid::default(),
         }
     }
 }
@@ -41,22 +42,22 @@ pub struct Grid {
 impl Default for Grid {
     fn default() -> Self {
         Self {
-                cell_size: 1,
-                cols: 5,
-                rows: 6
-            }
+            cell_size: 1,
+            cols: 5,
+            rows: 6,
         }
+    }
 }
 
 impl StatefulWidget for Grid {
-
     type State = WordleGrid;
 
-    fn render(self, area: Rect, buf: &mut ratatui::prelude::Buffer, state: &mut Self::State)
-    {
+    fn render(self, area: Rect, buf: &mut ratatui::prelude::Buffer, state: &mut Self::State) {
         // Define the contraints for every cell
-        let col_constraints = (0..self.cols).map(|_| Constraint::Length(self.cell_size as u16 + 4));
-        let row_constraints = (0..self.rows).map(|_| Constraint::Length(self.cell_size as u16 + 2));
+        let col_constraints =
+            (0..self.cols).map(|_| Constraint::Length(self.cell_size as u16 + 4));
+        let row_constraints =
+            (0..self.rows).map(|_| Constraint::Length(self.cell_size as u16 + 2));
 
         let horizontal = Layout::horizontal(col_constraints).spacing(1);
         let vertical = Layout::vertical(row_constraints);
@@ -64,26 +65,23 @@ impl StatefulWidget for Grid {
         let rows = vertical.split(area);
         for (row_index, &row_area) in rows.iter().enumerate() {
             for (col_index, &col_area) in horizontal.split(row_area).to_vec().iter().enumerate() {
-
                 let current_cell = state.grid[row_index][col_index];
 
-                Paragraph::new(Text::from(format!("{}", current_cell.letter
-                        .unwrap_or(' ')))
-                        .style(Style::new().fg(tailwind::WHITE)))
-                    .block(Block::bordered())
-                    .centered()
-                    .style(Style::new().fg(current_cell.color.into()))
-                    .render(col_area, buf);
+                Paragraph::new(
+                    Text::from(format!("{}", current_cell.letter.unwrap_or(' ')))
+                        .style(Style::new().fg(Color::White)),
+                )
+                .block(Block::bordered())
+                .centered()
+                .style(Style::new().fg(current_cell.color.into()))
+                .render(col_area, buf);
             }
         }
-
     }
 }
 
 impl App {
-
     pub async fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
-
         self.solution = get_daily_word().await.expect("Couldn't get daily word");
 
         loop {
@@ -91,19 +89,25 @@ impl App {
             if let Event::Key(key) = event::read()? {
                 if key.kind == KeyEventKind::Press {
                     match key.code {
-                        KeyCode::Enter =>
+                        KeyCode::Enter => {
+                            if WordleGrid::send_word(&mut self.content, &self.solution)
+                                && !self.has_won
                             {
-                                if WordleGrid::send_word(&mut self.content, &self.solution) && !self.has_won
-                                {
-                                    self.text_box = format!("You won!");
-                                    self.has_won = true;
-                                }
+                                self.text_box = format!("You won!");
+                                self.has_won = true;
                             }
-                        KeyCode::Backspace => if !self.has_won { WordleGrid::remove_char(&mut self.content)},
+                        }
+                        KeyCode::Backspace => {
+                            if !self.has_won {
+                                WordleGrid::remove_char(&mut self.content)
+                            }
+                        }
                         KeyCode::Esc => return Ok(()),
                         KeyCode::Char(c) => {
                             if !self.has_won {
-                                if !c.is_ascii_alphabetic() { continue; }
+                                if !c.is_ascii_alphabetic() {
+                                    continue;
+                                }
                                 self.content.append_char(c);
                             }
                         }
@@ -115,58 +119,49 @@ impl App {
     }
 
     fn draw(&mut self, frame: &mut Frame) {
-
         let grid = Grid::default();
 
-         let horizontal = &Layout::horizontal(
-             [Constraint::Fill(1),
-                          Constraint::Max((grid.cell_size as u16 + 5) * 5),
-                          Constraint::Fill(1)]);
+        let horizontal = &Layout::horizontal([
+            Constraint::Fill(1),
+            Constraint::Max((grid.cell_size as u16 + 5) * 5),
+            Constraint::Fill(1),
+        ]);
 
-         let rects = horizontal.split(frame.area());
+        let rects = horizontal.split(frame.area());
 
-         let inner_layout = &Layout::vertical(
-             [Constraint::Fill(1),
-                         Constraint::Length(4),
-                         Constraint::Min((grid.cell_size as u16 + 2) * 6),
-                         Constraint::Length(4),
-                         Constraint::Fill(1)])
+        let inner_layout = &Layout::vertical([
+            Constraint::Fill(1),
+            Constraint::Length(5),
+            Constraint::Min((grid.cell_size as u16 + 2) * 6),
+            Constraint::Length(4),
+            Constraint::Fill(1),
+        ])
         .split(rects[1]);
 
-         self.render_header(frame, inner_layout[1]);
-         frame.render_stateful_widget(grid, inner_layout[2], &mut self.content);
-         self.render_footer(frame, inner_layout[3]);
-     }
+        self.render_header(frame, inner_layout[1]);
+        frame.render_stateful_widget(grid, inner_layout[2], &mut self.content);
+        self.render_footer(frame, inner_layout[3]);
+    }
 
-
-     fn render_header(&self, frame: &mut Frame, area: Rect) {
-         let header = BigText::builder()
-                 .pixel_size(PixelSize::Quadrant)
-                 .style(Style::new().fg(tailwind::WHITE))
-                 .centered()
-                 .lines(vec![
-                     "Wordle".into()
-                 ])
-                 .build();
+    fn render_header(&self, frame: &mut Frame, area: Rect) {
+        let header = BigText::builder()
+            .pixel_size(PixelSize::Quadrant)
+            .style(Style::new().fg(Color::White))
+            .centered()
+            .lines(vec!["Wordle".into()])
+            .build();
 
         frame.render_widget(header, area);
-     }
+    }
 
-
-     fn render_footer(&self, frame: &mut Frame, area: Rect) {
-         let info_footer = Paragraph::new(self.text_box.to_owned())
-             .wrap(ratatui::widgets::Wrap { trim: false })
-             .style(
-                 Style::new()
-                     .fg(tailwind::WHITE)
-             )
-             .centered()
-             .block(
-                 Block::bordered()
-                     .border_style(Style::new().fg(tailwind::WHITE)),
-             );
-         frame.render_widget(info_footer, area);
-     }
-
-
+    fn render_footer(&self, frame: &mut Frame, area: Rect) {
+        let info_footer = Paragraph::new(self.text_box.to_owned())
+            .wrap(ratatui::widgets::Wrap { trim: false })
+            .style(Style::new().fg(Color::White))
+            .centered()
+            .block(
+                Block::bordered().border_style(Style::new().fg(Color::White)),
+            );
+        frame.render_widget(info_footer, area);
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,23 @@
 /* Library for the Wordle App */
-use ratatui::style::palette::tailwind;
+use chrono::Datelike;
 use reqwest::*;
-use chrono::{Datelike};
 
 #[derive(Debug, Clone, Copy, PartialEq, Default)]
 pub enum Color {
     Green,
     Yellow,
     Gray,
-    #[default] Blank
+    #[default]
+    Blank,
 }
 
 impl Into<ratatui::style::Color> for Color {
     fn into(self) -> ratatui::style::Color {
         match self {
-            Color::Blank => tailwind::NEUTRAL.c700,
-            Color::Green => tailwind::GREEN.c600,
-            Color::Gray =>  tailwind::GRAY.c500,
-            Color::Yellow => tailwind::YELLOW.c400
+            Color::Blank => ratatui::style::Color::DarkGray,
+            Color::Green => ratatui::style::Color::Green,
+            Color::Gray => ratatui::style::Color::Gray,
+            Color::Yellow => ratatui::style::Color::Yellow,
         }
     }
 }
@@ -25,17 +25,15 @@ impl Into<ratatui::style::Color> for Color {
 #[derive(Debug, Default, Copy, Clone)]
 pub struct WordleBox {
     pub letter: Option<char>,
-    pub color: Color
+    pub color: Color,
 }
 
 impl WordleBox {
-
     pub fn new(c: Option<char>, color: Color) -> WordleBox {
         WordleBox { letter: c, color }
     }
 
-    pub fn to_string(wboxes : [WordleBox; 5]) -> String {
-
+    pub fn to_string(wboxes: [WordleBox; 5]) -> String {
         let mut str = String::new();
         for w in wboxes {
             str.push(w.letter.expect("String empty found"));
@@ -45,21 +43,22 @@ impl WordleBox {
     }
 }
 
-
 pub struct WordleGrid {
     pub grid: [[WordleBox; 5]; 6],
-    first_free: (usize, usize)
+    first_free: (usize, usize),
 }
 
 impl Default for WordleGrid {
     fn default() -> WordleGrid {
-        WordleGrid { grid: [[WordleBox::new(None, Color::Blank); 5]; 6], first_free: (0,0) }
+        WordleGrid {
+            grid: [[WordleBox::new(None, Color::Blank); 5]; 6],
+            first_free: (0, 0),
+        }
     }
 }
 
 impl WordleGrid {
-
-    pub fn append_char(&mut self, c : char) {
+    pub fn append_char(&mut self, c: char) {
         if self.first_free.1 <= 4 {
             self.grid[self.first_free.0][self.first_free.1] = WordleBox::new(Some(c), Color::Blank);
             self.first_free.1 += 1;
@@ -71,16 +70,13 @@ impl WordleGrid {
             self.first_free.1 = self.first_free.1.saturating_sub(1);
             self.grid[self.first_free.0][self.first_free.1] = WordleBox::new(None, Color::Blank);
         }
-
     }
 
-    pub fn send_word(&mut self, solution : &str) -> bool {
-
+    pub fn send_word(&mut self, solution: &str) -> bool {
         let mut has_won = false;
 
         // Cannot send word if its not long five characters
         if self.first_free.1 == 5 {
-
             self.first_free.0 += 1;
 
             let input = WordleBox::to_string(self.grid[self.first_free.0.saturating_sub(1)]);
@@ -102,45 +98,45 @@ impl WordleGrid {
 }
 
 pub async fn get_daily_word() -> std::result::Result<String, reqwest::Error> {
-
     let today_date = chrono::Utc::now();
 
-    let fetch_word = get(format!("https://www.nytimes.com/svc/wordle/v2/{:04}-{:02}-{:02}.json",
-            today_date.year(),
-            today_date.month(),
-            today_date.day())
-        )
-        .await?
-        .text()
-        .await?;
-
+    let fetch_word = get(format!(
+        "https://www.nytimes.com/svc/wordle/v2/{:04}-{:02}-{:02}.json",
+        today_date.year(),
+        today_date.month(),
+        today_date.day()
+    ))
+    .await?
+    .text()
+    .await?;
 
     // It's better to remove the expects and use if lets because
     // we don't want our function to crash and just return an error
     // if it happens, and then handle it in the main program
-    let word = fetch_word.split(",")
+    let word = fetch_word
+        .split(",")
         .find(|x| x.contains("solution"))
         .expect("Couldn't find solution")
         .split_once(":")
         .expect("Split failed")
-        .1
-        [1..6]
+        .1[1..6]
         .to_owned();
 
     Ok(word)
 }
 
-
-pub fn check_word(input : &str, word : &str) -> [WordleBox; 5]
-{
-    let wboxes : Vec<WordleBox> = input.trim().chars().zip(word.chars())
+pub fn check_word(input: &str, word: &str) -> [WordleBox; 5] {
+    let wboxes: Vec<WordleBox> = input
+        .trim()
+        .chars()
+        .zip(word.chars())
         .map(|(i, w)| {
             if i == w {
-                return WordleBox::new(Some(i), Color::Green)
+                return WordleBox::new(Some(i), Color::Green);
             } else if word.contains(i) {
-                return WordleBox::new(Some(i), Color::Yellow)
+                return WordleBox::new(Some(i), Color::Yellow);
             } else {
-                return WordleBox::new(Some(i), Color::Gray)
+                return WordleBox::new(Some(i), Color::Gray);
             }
         })
         .collect();
@@ -148,7 +144,11 @@ pub fn check_word(input : &str, word : &str) -> [WordleBox; 5]
     let boxed_slice = wboxes.into_boxed_slice();
     let boxed_array: Box<[WordleBox; 5]> = match boxed_slice.try_into() {
         Ok(ba) => ba,
-        Err(o) => panic!("Expected a Vef of WordleBoxes of length {} but it was {}", 5, o.len()),
+        Err(o) => panic!(
+            "Expected a Vef of WordleBoxes of length {} but it was {}",
+            5,
+            o.len()
+        ),
     };
 
     *boxed_array
